b59157d5e1cb2755d9d84d27fe60804e64d2a2f3
wiiqwertyuiop <33212768+wiiqwertyuiop@users.noreply.github.com>
2019-01-24 20:09:58 -0500

    Add files via upload

--- ray.py
+++ ray.py
@@ -2,21 +2,23 @@ import math
 import pygame
 
 # Todo:
-# Combined ray intersection functions
+# Change coordinates so it makes sense
 # Different wall heights
+# Comments
 
 ################################
 
 # Window screen size
-screen = pygame.display.set_mode((520, 400))
+windowWidth = 620
+windowHeight = 500
 
 # Set player starting position in map
 StartingXCoordinate = 1.5
 StartingYCoordinate = 3.5
 
 # This is the size of the game screen INSIDE the window screen
-screenWidth = 520
-screenHeight = 400
+screenWidth = windowWidth
+screenHeight = windowHeight
 
 ################################
 # Map
@@ -41,6 +43,7 @@ mapSize = mapHeight*mapWidth
 
 blockSize = 64
 
+# Get map tile
 def MapTile(X, Y): 
   return map[ math.floor(Y/blockSize) ] [ math.floor(X/blockSize) ]
   
@@ -60,11 +63,51 @@ def tan(angle):
 
 
 ################################
-# Player attributes
+# main
+
+def main():
+
+  hfov = player.fov / 2
+  distanceToPlane = (screenWidth/2) / tan(hfov)
+
+  while update(): # loop until we quit
+
+    ray = Rays(player.dir - hfov)
+    
+    # loop through all columns casting rays
+    for x in range(0, screenWidth):
+      
+      # correct fisheye effect
+      ray.distance *= cos(player.dir - ray.angle)
+      
+      # get wall height
+      wallHeight = math.ceil(blockSize / ray.distance * distanceToPlane)
+      
+      # get block color
+      color = (255,255,255)
+      if ray.block == 2:
+        color = (0,0,255)
+      elif ray.block == 3:
+        color = (0,255,0)
+      elif ray.block == 4:
+        color = (0,127,180)
+      
+      # Y position of where to start the wall
+      screenYpos = screenHeight/1.75
+      # draw column
+      pygame.draw.line(screen, color, (x, screenYpos-(wallHeight/2)), (x, screenYpos+(wallHeight/2)))
+      
+      # Trace next ray
+      ray.next()
+
+################################
+# Handle player
 
 class Player():
 
   def __init__(self, playerXcoor, playerYcoor):
+  
+    ## set player starting attributes
 
     # 0-63 is first block
     # 64-128 is second block etc.
@@ -82,11 +125,13 @@ class Player():
     self.TurnSpeed = 0.5
     
     self.fov = 60
-    
+  
+  # Move player
   def update(self):
   
     keys = pygame.key.get_pressed()
     
+    # turn player
     if keys[pygame.K_RIGHT]:
       self.dir += self.TurnSpeed
       if self.dir > 360:
@@ -96,6 +141,7 @@ class Player():
       if self.dir < 0:
         self.dir += 360
     
+    # create test position
     X_temp = self.XPos
     Y_temp = self.YPos
     
@@ -106,141 +152,147 @@ class Player():
       X_temp -= cos(self.dir) * self.WalkSpeed
       Y_temp += sin(self.dir) * self.WalkSpeed
     
-    if MapTile(X_temp, self.YPos) == 0:
+    # if we would be in a wall don't put the player there
+    if not MapTile(X_temp, self.YPos):
       self.XPos = X_temp
 
-    if MapTile(self.XPos, Y_temp) == 0:
+    if not MapTile(self.XPos, Y_temp):
       self.YPos = Y_temp
       
 
+################################
+# Handles all rays
+
+class Rays:
+
+  ## init (also cast the first ray)
+  def __init__(self, startingAngle):
+    
+    if startingAngle < 0:
+      startingAngle += 360
+    
+    self.angle = startingAngle
+    
+    # This is the distance to the wall the ray is touching
+    self.distance = 0
+
+    # This is the block the ray is touching
+    self.block = 0
+    
+    self.Cast()
+  
+  ## Cast next ray
+  def next(self):
+    
+    angleBetweenColumns = player.fov/screenWidth
+    self.angle += angleBetweenColumns
+    
+    if self.angle > 360:
+      self.angle -= 360 
+      
+    self.Cast() 
+   
+  ## Ray tracing routine
+  def Cast(self):
+    
+    # Horizontal and vertical intersections are checked seperately
+    # Remeber X increase to the right and Y increase DOWN *not* up
+    
+    # Setup for checking horizontal intersections
+    if self.angle > 0 and self.angle < 180:
+      HorzYpos = math.floor(player.YPos/blockSize) * blockSize - 1 # Y pos of ray
+      HorzYinc = -blockSize # amount to increase Y pos by to get to next block
+    else:
+      HorzYpos = math.floor(player.YPos/blockSize) * blockSize + blockSize
+      HorzYinc = blockSize
+    
+    HorzXpos = player.XPos + (player.YPos - HorzYpos)/tan(self.angle) # x pos of ray
+    HorzXinc = (-HorzYinc) / tan(self.angle) # amount to increase X position by to get to next block
+  
+    # Vertical check
+    if self.angle < 90 or self.angle > 270:
+      VertXpos = math.floor(player.XPos/blockSize) * blockSize + blockSize
+      VertXinc = blockSize
+    else:
+      VertXpos = math.floor(player.XPos/blockSize) * blockSize - 1
+      VertXinc = -blockSize
+    
+    VertYpos = player.YPos + (player.XPos - VertXpos)*tan(self.angle)
+    VertYinc = (-VertXinc) * tan(self.angle)
+    
+    ## Now we loop and check for contact with walls
+    
+    # record seperate distances and blocks hit for each intersections
+    distanceX = 0
+    distanceY = 0
+    
+    blockX = 0
+    blockY = 0
+    
+    # Loop until both checks hit a wall
+    while (blockX == 0) or (blockY == 0):
+    
+      if (blockX == 0):
+        
+        # Make sure the ray is within the map
+        # alternative to try/except would be:
+        # HorzX/blockSize * HorzY/blockSize < mapSize
+        try:
+          blockX = MapTile(HorzXpos, HorzYpos)
+          if blockX: distanceX = math.fabs( (player.XPos-HorzXpos)/cos(self.angle) )
+        except:
+          blockX = 1
+          distanceX = 9999
+            
+        HorzXpos += HorzXinc
+        HorzYpos += HorzYinc
+      
+      # Stop if we already hit a wall on this intersection
+      if (blockY == 0):
+      
+          try:
+            blockY = MapTile(VertXpos, VertYpos)
+            if blockY: distanceY = math.fabs( (player.XPos-VertXpos)/cos(self.angle) )
+          except:
+            blockY = 1
+            distanceY = 9999
+          
+          VertXpos += VertXinc
+          VertYpos += VertYinc
+    
+    # Use whichever wall is closer
+    if distanceX < distanceY:
+      self.distance = distanceX
+      self.block = blockX
+    else:
+      self.distance = distanceY
+      self.block = blockY
+           
+#
+################################
+# update screen
+
+def update():
+
+    player.update()
+    pygame.display.flip()
+    screen.fill((0,0,0))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+                return False
+    # keep running
+    return True
 
 ################################
-#
-
-def CheckHorz(rayAngle):
-    
-  if rayAngle > 0 and rayAngle < 180:
-    HorzY = math.floor(player.YPos/blockSize) * blockSize - 1
-    Ya = -blockSize
-  else:
-    HorzY = math.floor(player.YPos/blockSize) * blockSize + blockSize
-    Ya = blockSize
-  
-  HorzX = player.XPos + (player.YPos - HorzY)/tan(rayAngle)
-  
-  Xa = (-Ya) / tan(rayAngle)
-  
-  while True:
-    
-    # alternative to try/except would be:
-    # HorzX/blockSize * HorzY/blockSize < mapSize
-    try:
-      # Make sure the ray is within the map
-      tile = MapTile(HorzX, HorzY)
-    except:
-      return 9999, 0
-    
-    if (tile != 0):
-      return math.fabs( (player.XPos-HorzX)/cos(rayAngle) ), tile
-        
-    HorzX += Xa
-    HorzY += Ya
-    
-
-def CheckVert(rayAngle):
-  
-  # Remeber X increase to the right and Y increase DOWN *not* up
-  if rayAngle < 90 or rayAngle > 270:
-    VertX = math.floor(player.XPos/blockSize) * blockSize + blockSize
-    Xa = blockSize
-  else:
-    VertX = math.floor(player.XPos/blockSize) * blockSize - 1
-    Xa = -blockSize
-  
-  VertY = player.YPos + (player.XPos - VertX)*tan(rayAngle)
-  Ya = (-Xa) * tan(rayAngle)  # note: this is fucking stupid how Y decrease as we go up. Change this in the future
-  
-  while True:
-    
-    try:
-      tile = MapTile(VertX, VertY)
-    except:
-      return 9999, 0
-    
-    if (tile != 0):
-      return math.fabs( (player.XPos-VertX)/cos(rayAngle) ), tile
-    
-    VertX += Xa
-    VertY += Ya
-    
-def TraceRay(rayAngle):
-
-  distanceX, blockX = CheckHorz(rayAngle)
-  distanceY, blockY = CheckVert(rayAngle)
-  
-  if distanceX < distanceY:
-    return distanceX, blockX
-  else:
-    return distanceY, blockY
-  
-        
-#
-################################
+# init
 
 pygame.init()
 pygame.display.set_caption("Raycaster")
+screen = pygame.display.set_mode((windowWidth, windowHeight))
 
-################################
-# main
- 
+# create a player
 player = Player(StartingXCoordinate, StartingYCoordinate)
 
-hfov = player.fov / 2
-distanceToPlane = (screenWidth/2) / tan(hfov)
-angleBetweenColumns = player.fov/screenWidth
-
-done = False
-while not done:
-
-  rayAngle = player.dir - hfov
-  if rayAngle < 0:
-    rayAngle += 360
-  
-  for x in range(0, screenWidth):
-     
-    distance, block = TraceRay(rayAngle)
-    
-    # correct fisheye effect
-    distance *= cos(player.dir - rayAngle)
-    wallHeight = math.ceil(blockSize / distance * distanceToPlane)
-    
-    color = (255,255,255)
-    if block == 2:
-      color = (0,0,255)
-    elif block == 3:
-      color = (0,255,0)
-    elif block == 4:
-      color = (0,127,180)
-    
-    # Y position of where to start the wall
-    screenYpos = screenHeight/1.75
-    
-    pygame.draw.line(screen, color, (x, screenYpos-(wallHeight/2)), (x, screenYpos+(wallHeight/2)))
-    
-    rayAngle += angleBetweenColumns
-    if rayAngle > 360:
-      rayAngle -= 360    
-  
-  
-  player.update()
-  pygame.display.flip()
-  screen.fill((0,0,0))
-  
-  for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-              done = True
-            
-
-################################
-
+main() # run main
 pygame.quit()
